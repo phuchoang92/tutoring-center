58d72e271366cb629457fa32fe1d5c4171cc939c
phuchoang29 <changchayabcd@gmail.com>
2025-03-18 22:03:38 +0700

    start

---
+++ src/main/java/com/phuc/tutoring_center/TutoringCenterApplication.java
@@ -0,0 +1,13 @@
+package com.phuc.tutoring_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoringCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoringCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phuc/tutoring_center/TutoringCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuc.tutoring_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoringCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
